e231a3e725a018cda98b2f7967e516684a3a8648
MJ-KOLLtd <mjhabalmusic@gmail.com>
2026-06-06 02:37:08 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://esquareestates.com', // TODO: update with your real domain
-  integrations: [sitemap()],
 });
