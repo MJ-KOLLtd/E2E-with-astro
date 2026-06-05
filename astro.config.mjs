12244d653105d286407f69f615431e8a7dce9008
MJ-KOLLtd <mjhabalmusic@gmail.com>
2026-06-06 02:44:19 +0800

    fix sitemap build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
+﻿import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://esquareestates.com', // TODO: update with your real domain
+  site: 'https://esquareestates.com',
 });
